cbff6f60fb1d1536db67869803fe99e076c22bf5
Franco M <damianmaciel0@gmail.com>
2022-07-05 19:00:55 -0300

    Se agrego una variable de entorno para la url de la AIP

--- services/GET_DOG.js
+++ services/GET_DOG.js
@@ -1,6 +1,7 @@
+import { API_URL } from "./API_URL"
 export const GET_DOG = async (id) => {
     try {
-        const response = await fetch(`http://localhost:3001/dogs/${id}`)
+        const response = await fetch(`${API_URL}/dogs/${id}`)
         const dogs = response.json()
         return dogs
     } catch (error) {
